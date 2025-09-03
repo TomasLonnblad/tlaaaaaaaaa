5aaf567778a9f585a192bc7b377b2d48ea9e0e2a
Tomas <tomaslonnblad@gmail.com>
2025-09-03 21:40:35 +0200

    Update index.vue

--- index.vue
+++ index.vue
@@ -3,9 +3,14 @@
     Go to
     
 
+<template>
+  <NuxtLink :to="{ name: 'posts-id', params: { id: 123 } }">
+    Post 123
+  </NuxtLink>
+</template>
 
 
-<NuxtLink to="/about">About page</NuxtLink>
+    
     
   </div>
 </template>

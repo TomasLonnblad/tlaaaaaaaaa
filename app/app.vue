b7afd28b6a0699718edf73a777c5cf5c126bd321
Tomas <tomaslonnblad@gmail.com>
2025-09-03 22:38:51 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -2,27 +2,22 @@
 
 
 <script setup lang="ts">
-const version = 2 + 1
+const page = ref()
+
+function logFoo () {
+  page.value.pageRef.foo()
+}
 </script>
 
-
-
-
-
 <template>
-  <NuxtLink to="https://twitter.com/nuxt_js">
-    Nuxt Twitter
-  </NuxtLink>
-  <!-- <a href="https://twitter.com/nuxt_js" rel="noopener noreferrer">...</a> -->
-
-  <NuxtLink to="https://discord.nuxtjs.org" rel="noopener">
-    Nuxt Discord
-  </NuxtLink>
-  <!-- <a href="https://discord.nuxtjs.org" rel="noopener">...</a> -->
-
-  <NuxtLink to="/about" target="_blank">About page</NuxtLink>
-  <!-- <a href="/about" target="_blank" rel="noopener noreferrer">...</a> -->
+  <NuxtPage ref="page" />
 </template>
+
+
+
+
+
+
 
 
 

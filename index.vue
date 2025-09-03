7e63b47b79059a95f76778e15e6fa23ae31e2736
Tomas <tomaslonnblad@gmail.com>
2025-09-03 21:58:56 +0200

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,19 +1,25 @@
 <template>
-  <div>
+
     
     
 
 
   
-
-<NuxtLink to="https://nuxtjs.org">
-    Nuxt website
+<NuxtLink to="https://twitter.com/nuxt_js">
+    Nuxt Twitter
   </NuxtLink>
-  <!-- <a href="https://nuxtjs.org" rel="noopener
+  <!-- <a href="https://twitter.com/nuxt_js" rel="noopener noreferrer">...</a> -->
 
+  <NuxtLink to="https://discord.nuxtjs.org" rel="noopener">
+    Nuxt Discord
+  </NuxtLink>
+  <!-- <a href="https://discord.nuxtjs.org" rel="noopener">...</a> -->
+
+  <NuxtLink to="/about" target="_blank">About page</NuxtLink>
+  <!-- <a href="/about" target="_blank" rel="noopener noreferrer">...</a> -->
 
     
     
     
-  </div>
+  
 </template>

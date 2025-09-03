ab703d421951d80ce2b66759566ee0b6f96d2469
Tomas <tomaslonnblad@gmail.com>
2025-09-03 22:20:26 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -7,21 +7,23 @@ const version = 2 + 1
 
 
 
-<template>
-  <div class="hello">
-    Hello Nuxt {{ version }} and Tomas with extended module!
-  </div>
-</template>
-
-
 
 
 <template>
-  <NuxtLink to="https://nuxtjs.org">
-    Nuxt website
+  <NuxtLink to="https://twitter.com/nuxt_js">
+    Nuxt Twitter
   </NuxtLink>
-  <!-- <a href="https://nuxtjs.org" rel="noopener noreferrer">...</a> -->
+  <!-- <a href="https://twitter.com/nuxt_js" rel="noopener noreferrer">...</a> -->
+
+  <NuxtLink to="https://discord.nuxtjs.org" rel="noopener">
+    Nuxt Discord
+  </NuxtLink>
+  <!-- <a href="https://discord.nuxtjs.org" rel="noopener">...</a> -->
+
+  <NuxtLink to="/about" target="_blank">About page</NuxtLink>
+  <!-- <a href="/about" target="_blank" rel="noopener noreferrer">...</a> -->
 </template>
+
 
 
 
